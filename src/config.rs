3e5aad7e3a4a01813144ba4c3e1d832dd063a750
Mateus Melchiades <mateusbmelchiades@gmail.com>
2022-10-07 14:19:24 -0300

    Update gitignore

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
 pub static VERSION: &str = "0.1.0";
 pub static GETTEXT_PACKAGE: &str = "beedget";
-pub static LOCALEDIR: &str = "/usr/local/share/locale";
-pub static PKGDATADIR: &str = "/usr/local/share/beedget";
+pub static LOCALEDIR: &str = "/home/matbme/Documents/Beedget/appdata/share/locale";
+pub static PKGDATADIR: &str = "/home/matbme/Documents/Beedget/appdata/share/beedget";
